7789ba1774fc7c844ffd1b1046af6fa527ed00f9
Евгений Есауленко <esaulenko.lenovo@gmail.com>
2016-02-13 21:25:27 +0300

    Fixing exceptions

--- ConsoleCalculator/ConsoleCalculator/Program.cs
+++ ConsoleCalculator/ConsoleCalculator/Program.cs
@@ -6,8 +6,8 @@ namespace ConsoleCalculator
     class Program
     {
         private const string WELCOME_TEXT = "Console Calculator 1.00\nAvailable operations: sum(+), sub(-), mult(*), div(/)\nAvailable variables: int, double\nExample command: sum 3 2.54 18\n";
-        private const string ERROR_OPERATION = "Wrong operation. Available operations: sum, sub, mult, div\n";
-        private const string ERROR_DATA = "Invalid data. Available variables: int, double";
+        private const string ERROR_EMPTY = "Empty command. Available operations: sum, sub, mult, div\n";
+        private const string ERROR_WRONG = "Wrong command. Available operations: sum, sub, mult, div. Available variables: int, double\n";
 
         static void Main(string[] args)
         {
@@ -22,14 +22,34 @@ namespace ConsoleCalculator
                 Console.WriteLine("Enter the command:");
                 string input = Console.ReadLine();
 
+                //Checking empty string
                 if (string.IsNullOrWhiteSpace(input))
                 {
-                    Console.WriteLine(ERROR_OPERATION);
+                    Console.WriteLine(ERROR_EMPTY);
                     continue;
                 }
                 
                 string[] splited = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
                 string operation = splited[0];
+
+                //Checking number of elements in the array
+                if (splited.Length < 2)
+                {
+                    Console.WriteLine(ERROR_WRONG);
+                    continue;
+                }
+
+                //Checking correctness of numerical data
+                double checkValue;
+                for (int i = 1; i < splited.Length; i++)
+                {
+                    if (!double.TryParse(splited[i], NumberStyles.Number, CultureInfo.InvariantCulture, out checkValue))
+                    {
+                        Console.WriteLine(ERROR_WRONG);
+                        Calculator();
+                    }
+                }
+
                     switch (operation)
                     {
                         case "sum":
@@ -45,7 +65,7 @@ namespace ConsoleCalculator
                             Calculations(splited, "div");
                             break;
                         default:
-                        Console.WriteLine(ERROR_OPERATION);
+                        Console.WriteLine(ERROR_WRONG);
                         break;
                     }
             } 
