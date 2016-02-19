839ed03c8bef457091a0a577ed2b39559a412011
Евгений Есауленко <esaulenko.lenovo@gmail.com>
2016-02-19 23:35:23 +0300

    Minor fix

--- ConsoleCalculator/ConsoleCalculator/Operations/OperationBase.cs
+++ ConsoleCalculator/ConsoleCalculator/Operations/OperationBase.cs
@@ -5,25 +5,19 @@ namespace ConsoleCalculator
 {
     abstract class OperationBase
     {
-        double checkedValue;
-        bool isValid = true;
-
         //Checking correctness of numerical data
         public virtual bool CheckValues(string[] str)
         {
+            double checkedValue;
+
             for (int i = 1; i < str.Length; i++)
             {
                 if (!double.TryParse(str[i], NumberStyles.Number, CultureInfo.InvariantCulture, out checkedValue))
                 {
                     Console.WriteLine(OperationProcessor.ERROR_WRONG);
-                    isValid = false;
-                    break;
+                    return false;
                 }
             }
-
-            if (!isValid)
-                return false;
-            else
                 return true;
         }
         public abstract double GetResult(string[] splitedString);
